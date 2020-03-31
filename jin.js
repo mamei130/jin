bbc1f57564119e739c7951fe7bcd552dd0e334b4
mamei130 <60641247+mamei130@users.noreply.github.com>
2020-03-31 18:42:00 +0800

    Add files via upload

---
+++ jin.js
@@ -0,0 +1,38 @@
+$(function() {
+  var topBtn = $('#page-top');    
+  topBtn.hide();
+  //スクロールが100に達したらボタン表示
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 650) {
+          topBtn.fadeIn();
+      } else {
+          topBtn.fadeOut();
+      }
+  });
+  //スクロールしてトップ
+  topBtn.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 300);
+      return false;
+  });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+// $(function(){
+//   $('#page-top').click(function(){
+//     $('html, body').animate({'scrollTop':0},
+//     '500');
+//   });
+
+// });
